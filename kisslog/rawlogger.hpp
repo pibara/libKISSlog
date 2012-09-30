0d7408c2b9078dfd168bd25a96d26159b201d6e3
Rob Meijer <rob@Annalies>
2012-09-30 22:54:06 +0200

    Added preliminary code for syslog protocol stuff.

--- kisslog/rawlogger.hpp
+++ kisslog/rawlogger.hpp
@@ -2,3 +2,6 @@
 #ifndef WIN32
 #include <kisslog/rawlogger/sysloglogger.hpp>
 #endif
+#if defined(__GXX_EXPERIMENTAL_CXX0X__) || __cplusplus >= 201103L
+//#include <kisslog/rawlogger/syslogprotologger.hpp>
+#endif
